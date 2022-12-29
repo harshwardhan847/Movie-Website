41d162a60e0903c6a47a761dcb17f17552fe9418
Harsh <harshwardhan847@gmail.com>
2022-12-29 11:30:44 +0530

    add new page to open each page information

--- script.js
+++ script.js
@@ -14,75 +14,96 @@ const main = document.getElementById("main");
 const form = document.getElementById("search_form");
 const search = document.getElementById("search");
 const logo = document.querySelector(".logo");
+const movieCard = document.querySelector(".movie");
 
-'use strict';
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=368ada029f20db0a7a4a9cfb469ee031&page=1'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+("use strict");
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=368ada029f20db0a7a4a9cfb469ee031&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_URL =
   'https://api.themoviedb.org/3/search/movie?api_key=368ada029f20db0a7a4a9cfb469ee031&query="';
 
-async function getMovies(url){
-    const res = await fetch(uri);
-    const data = await res.json();
-    if(data.results.length === 0){
-        main.innerHTML = '';
-        const createErrorEl = document.createElement('div')
-        createErrorEl.classList.add('errorHandle')
-        createErrorEl.innerHTML = `<h1>Oh no 🙅🏻 ! There is no such movie exists</h1>`;
-        main.appendChild(createErrorEl);
-    }
-    else{
-        showMovies(data.results);
-    }
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  if (data.results.length === 0) {
+    main.innerHTML = "";
+    const createErrorEl = document.createElement("div");
+    createErrorEl.classList.add("errorHandle");
+    createErrorEl.innerHTML = `<h1>Oh no 🙅🏻 ! There is no such movie exists</h1>`;
+    main.appendChild(createErrorEl);
+  } else {
+    showMovies(data.results);
+  }
 }
 
-function showMovies(movies){
-    main.innerHTML = ''
+function showMovies(movies) {
+  main.innerHTML = "";
 
-    movies.forEach((movie)=>{
-        const {title,poster_path,vote_average,overview} = movie;
-        const createEl = document.createElement('div');
-        createEl.classList.add('movie');
+function gotoSingle(){
+    window.location.href = "single.html"
+}
 
-        createEl.innerHTML = `
-        <img src="${IMG_PATH + poster_path}" alt="movie image" class="movie-img" />
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview} = movie;
+    const createEl = document.createElement("div");
+    createEl.classList.add("movie");
+    createEl.id = movie.id;
+    // console.log(createEl.id)
+    
+
+    createEl.addEventListener('click',()=>{
+      // console.log(movie);
+      gotoSingle();
+      // console.log("next Page")
+      localStorage.setItem('id',createEl.id) 
+      // console.log("clicked "+createEl.id)
+      
+    })
+    
+    createEl.innerHTML = `
+        <img src="${
+          IMG_PATH + poster_path
+        }" alt="movie image" class="movie-img" />
         <div class="movie-info">
           <h3>${title}</h3>
           <span class="${getOverviewRating(
             vote_average
           )}">${vote_average}</span>
         </div>
-        <div class="overview">
-          <h3 class="overview">
-            ${overview}
-          </h3>
-        </div>
         `;
-        main.appendChild(createEl);
-    });
+    main.appendChild(createEl);
+    // console.log(movie)
+  });
 }
 
-
-function getOverviewRating(rating){
-   if (rating >= 8) {
-    return 'green';
+function getOverviewRating(rating) {
+  if (rating >= 8) {
+    return "green";
   } else if (rating >= 5) {
-    return 'orange';
+    return "orange";
   } else {
-    return 'red';
+    return "red";
   }
 }
 
+getMovies(API_URL);
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  const searchTerm = search.value;
 
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_URL + searchTerm);
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
 
-
-
-
-
-
-
-
-
-
+logo.addEventListener("click", () => {
+  main.innerHTML = "";
+  getMovies(API_URL);
+});
---
+++ scriptSingle.js
@@ -0,0 +1,81 @@
+
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
+let id = localStorage.getItem('id');
+let container = document.querySelector('.container');
+
+
+let url_id = 'https://api.themoviedb.org/3/movie/'+id+'?api_key=368ada029f20db0a7a4a9cfb469ee031'
+// console.log(url_id);
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    // console.log(data)
+    if (data.length === 0) {
+      main.innerHTML = "";
+      const createErrorEl = document.createElement("div");
+      createErrorEl.classList.add("errorHandle");
+      createErrorEl.innerHTML = `<h1>Oh no 🙅🏻 ! There is no such movie exists</h1>`;
+      main.appendChild(createErrorEl);
+    } else {
+        // console.log(data)
+      showMovies(data);
+    }
+  }
+// getMovies(url_id)
+
+  function showMovies(movie) {
+    const mainSingle = document.getElementById('single-main')
+    mainSingle.innerHTML = "";
+    console.log(movie)
+    // createEl.classList.add("movie");
+    let img = IMG_PATH+movie.backdrop_path
+    let poster = IMG_PATH+movie.poster_path
+    console.log(img)
+    mainSingle.innerHTML = `
+    <h1 class="original_title">${movie.original_title}</h1>
+    <img src="${img}" alt="Backdrop Image" class="backdrop">
+    <div class="container flex main-content">
+        <div class="original_language"><span>Language : </span>${movie.original_language}</div>
+        <div class="status"><span>Status :</span>${movie.status}</div>
+        <div class="overview-single"><span>Overview : </span>${movie.overview}</div>
+        <div class="budget"><span>Budget : </span>${movie.budget}</div>
+        <div class="tagline"><span>Tagline : </span>${movie.tagline}</div>
+        <div class="vote_average ${getOverviewRating(movie.vote_average)}"><span>Rating : </span>${movie.vote_average}</div>
+        <div class="vote_count"><span>Vote Count : </span>${movie.vote_count}</div>
+        <div class="genres"><span>geners:</span>${movie.genres[0].name} ${movie.genres[1].name} ${movie.genres[2].name}</div>
+        <div class="production_companies">
+
+        </div>
+        <div class="production_countries">
+
+        </div>
+        <div class="release_date"><span>release date : </span>${movie.release_date}</div>
+        <div class="revenue"><span>Revenue : </span>${movie.revenue}</div>
+        <div class="runtime"><span>runtime : </span>${movie.runtime}min</div>
+        
+        <img src="${poster}" alt="" class="poster">
+        <a href="${movie.homepage}" class="watchNow primary-button">Watch Now</a>
+
+        
+        
+    </div>
+
+
+
+`;
+  }
+  
+  function getOverviewRating(rating) {
+    if (rating >= 8) {
+      return "green";
+    } else if (rating >= 5) {
+      return "orange";
+    } else {
+      return "red";
+    }
+  }
+  getMovies(url_id)
+
+
+  // container.innerHTML = `<div>${id}</div>`;
